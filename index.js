6154f371533ca26f004045b134abee8c89a6a594
AbdullahT <83435812+AbdullahTh7@users.noreply.github.com>
2022-08-28 21:12:33 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+$('.section').on('click',function(){
+    var a = this.nextElementSibling;
+    console.log(a);
+    var text = $('p',this).css('display');
+    if (text == 'none') {
+        $('p',this).css('display','block');
+        $('h5',this).css('font-weight','700');
+    }
+    else{
+        $('p',this).css('display','none');
+        $('h5',this).css('font-weight','400');
+    }
+});
